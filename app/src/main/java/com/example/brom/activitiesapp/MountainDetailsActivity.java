d395faab099b0907d837d9ebfb2fcbccedeb685f
a17robda <a17robda@student.his.se>
2018-04-17 00:28:33 +0200

    0.1 Safe rollback intent

---
+++ app/src/main/java/com/example/brom/activitiesapp/MountainDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.example.brom.activitiesapp;
+
+import android.content.Intent;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MountainDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mountaindetails);
+    }
+
+
+}
